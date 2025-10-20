5ac9b76b1afe2bcf63bd9a791a8511083e93bc78
lakshmihr15 <1rn21ai159.lakshmihr@rnsit.ac.in>
2025-10-20 13:03:42 -0400

    Cleaned Django project, fixed Flake8 issues, added proper gitignore

--- newproject/settings.py
+++ newproject/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -24,10 +23,11 @@ SECRET_KEY = 'django-insecure-ote)mm9438e(i42e@nj9-(!^jhu3&96n+anun!*-4#!$8ul@-+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".elasticbeanstalk.com"]
-
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".elasticbeanstalk.com",
+]
 
 # Application definition
 
@@ -52,7 +52,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'newproject.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -67,24 +66,17 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'newproject.wsgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -99,27 +91,16 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = '/static/'
-import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+# Create your tests here.from django.test import TestCase
+from .models import Question
 
-# Create your tests here.
+
+class QuestionModelTests(TestCase):
+    def test_str_returns_question_text(self):
+        q = Question(question_text="Is this used?", pub_date=None)
+        self.assertEqual(str(q), "Is this used?")
